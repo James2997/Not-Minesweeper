84ea9d0d6dac2e42a79832ee58cf511ee3480537
James Pendley <pendley2997@gmail.com>
2022-11-17 09:37:10 -0500

    Initial Commit

--- app/src/main/java/com/mobileappdev/myapplication/Help.java
+++ app/src/main/java/com/mobileappdev/myapplication/Help.java
@@ -1,13 +1,14 @@
 package com.mobileappdev.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Help extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help);
     }
 }
