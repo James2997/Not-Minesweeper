33965d04fe93ad1268d7317b4f65e02397944337
James Pendley <pendley2997@gmail.com>
2022-11-27 21:28:22 -0500

    Fully implemented game images

--- app/src/main/java/com/mobileappdev/myapplication/ImageProcessing.java
+++ app/src/main/java/com/mobileappdev/myapplication/ImageProcessing.java
@@ -6,9 +6,27 @@ import android.graphics.BitmapFactory;
 import java.util.HashMap;
 
 public class ImageProcessing {
-    public static HashMap<String, Bitmap> processSprites() {
-        Bitmap original = BitmapFactory.decodeFile("@drawable/minesweeper");
+    public static HashMap<String, Bitmap> processSprites(Bitmap original) {
         HashMap<String, Bitmap> sprites = new HashMap<String, Bitmap>();
+
+        int height = original.getHeight()/4;
+        int width = original.getWidth()/4;
+
+        sprites.put("empty",    Bitmap.createBitmap(original, width * 0,0, width, height));
+        sprites.put("one",      Bitmap.createBitmap(original, width * 1,0, width, height));
+        sprites.put("two",      Bitmap.createBitmap(original, width * 2,0, width, height));
+        sprites.put("three",    Bitmap.createBitmap(original, width * 3,0, width, height));
+        sprites.put("four",     Bitmap.createBitmap(original, width * 0,height * 1, width, height));
+        sprites.put("five",     Bitmap.createBitmap(original, width * 1,height * 1, width, height));
+        sprites.put("six",      Bitmap.createBitmap(original, width * 2,height * 1, width, height));
+        sprites.put("seven",    Bitmap.createBitmap(original, width * 3,height * 1, width, height));
+        sprites.put("eight",    Bitmap.createBitmap(original, width * 0,height * 2, width, height));
+        sprites.put("unpressed",Bitmap.createBitmap(original, width * 1,height * 2, width, height));
+        sprites.put("bomb_hit", Bitmap.createBitmap(original, width * 2,height * 2, width, height));
+        sprites.put("flag",     Bitmap.createBitmap(original, width * 3,height * 2, width, height));
+        sprites.put("not_bomb", Bitmap.createBitmap(original, width * 0,height * 3, width, height));
+        sprites.put("question", Bitmap.createBitmap(original, width * 1,height * 3, width, height));
+        sprites.put("bomb",     Bitmap.createBitmap(original, width * 2,height * 3, width, height));
 
         return sprites;
     }
--- app/src/main/java/com/mobileappdev/myapplication/MainActivity.java
+++ app/src/main/java/com/mobileappdev/myapplication/MainActivity.java
@@ -2,19 +2,25 @@ package com.mobileappdev.myapplication;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String GAME_STATE = "gameState";
+    private HashMap<String, Bitmap> mSprites;
     private MineGame mGame;
     private GridLayout mGameGrid;
 
@@ -22,43 +28,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSprites = ImageProcessing.processSprites(BitmapFactory.decodeResource(getResources(), R.drawable.minesweeper));
 
         ActionBar.LayoutParams layout = new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layout.width = (int) ((270/ MineGame.GRID_WIDTH) * getResources().getDisplayMetrics().density + 0.5f);
         layout.height = (int) ((270/ MineGame.GRID_WIDTH) * getResources().getDisplayMetrics().density + 0.5f);
-        layout.setMargins(0, 0, 0, 0);
         mGameGrid = findViewById(R.id.game_grid);
         mGameGrid.setColumnCount(MineGame.GRID_WIDTH);
         mGameGrid.setRowCount(MineGame.GRID_HEIGHT);
         mGameGrid.setUseDefaultMargins(false);
 
         for (int i = 0; i < MineGame.GRID_WIDTH * MineGame.GRID_HEIGHT; i++) {
-            Button newButton = new Button(this);
+            ImageButton newButton = new ImageButton(this);
+            newButton.setImageBitmap(mSprites.get("unpressed"));
+            newButton.setPadding(0, 0, 0, 0);
+            newButton.setScaleType(ImageView.ScaleType.FIT_CENTER);
             newButton.setLayoutParams(layout);
             mGameGrid.addView(newButton);
         }
 
         for (int buttonIndex = 0; buttonIndex < mGameGrid.getChildCount(); buttonIndex++) {
-            Button gridButton = (Button) mGameGrid.getChildAt(buttonIndex);
+            ImageButton gridButton = (ImageButton) mGameGrid.getChildAt(buttonIndex);
             gridButton.setOnClickListener(this::onButtonClick);
             if(buttonIndex == 0)
                 gridButton.setOnLongClickListener(this::onButtonLongClick);
         }
 
         mGame = new MineGame();
-
-        if(savedInstanceState == null) {
-            startGame();
-        } else {
-            String gameState = savedInstanceState.getString(GAME_STATE);
-            mGame.setState(gameState);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(GAME_STATE, mGame.getState());
     }
 
     private void startGame() {
