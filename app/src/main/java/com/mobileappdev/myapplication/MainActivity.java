ba60cc5269a74680b553d5103e3c36383586c095
Arriaga124 <acristian46@gmail.com>
2022-11-29 09:55:25 -0500

    setup mine grid

--- app/src/main/java/com/mobileappdev/myapplication/MainActivity.java
+++ app/src/main/java/com/mobileappdev/myapplication/MainActivity.java
@@ -4,16 +4,13 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.HashMap;
@@ -31,14 +28,14 @@ public class MainActivity extends AppCompatActivity {
         mSprites = ImageProcessing.processSprites(BitmapFactory.decodeResource(getResources(), R.drawable.minesweeper));
 
         ActionBar.LayoutParams layout = new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layout.width = (int) ((270/ MineGame.GRID_WIDTH) * getResources().getDisplayMetrics().density + 0.5f);
-        layout.height = (int) ((270/ MineGame.GRID_WIDTH) * getResources().getDisplayMetrics().density + 0.5f);
+        layout.width = (int) ((270/ MineGame.GRID_HEIGHT) * getResources().getDisplayMetrics().density + 0.5f);
+        layout.height = (int) ((270/ MineGame.GRID_HEIGHT) * getResources().getDisplayMetrics().density + 0.5f);
         mGameGrid = findViewById(R.id.game_grid);
-        mGameGrid.setColumnCount(MineGame.GRID_WIDTH);
-        mGameGrid.setRowCount(MineGame.GRID_HEIGHT);
+        mGameGrid.setColumnCount(MineGame.GRID_HEIGHT);
+        mGameGrid.setRowCount(MineGame.GRID_WIDTH);
         mGameGrid.setUseDefaultMargins(false);
 
-        for (int i = 0; i < MineGame.GRID_WIDTH * MineGame.GRID_HEIGHT; i++) {
+        for (int i = 0; i < MineGame.GRID_HEIGHT * MineGame.GRID_WIDTH; i++) {
             ImageButton newButton = new ImageButton(this);
             newButton.setImageBitmap(mSprites.get("unpressed"));
             newButton.setPadding(0, 0, 0, 0);
@@ -68,14 +65,15 @@ public class MainActivity extends AppCompatActivity {
     private void onButtonClick(View view) {
 
         int buttonIndex = mGameGrid.indexOfChild(view);
-        int row = buttonIndex / MineGame.GRID_WIDTH;
-        int col = buttonIndex % MineGame.GRID_HEIGHT;
+        int row = buttonIndex / MineGame.GRID_HEIGHT;
+        int col = buttonIndex % MineGame.GRID_WIDTH;
 
-        mGame.selectLight(row, col);
+//        mGame.selectLight(row, col);
+//
+//        if (mGame.isGameOver()) {
+//            Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show();
+//        }
 
-        if (mGame.isGameOver()) {
-            Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show();
-        }
     }
 
     public void onHelpClick(View view) {
@@ -85,5 +83,53 @@ public class MainActivity extends AppCompatActivity {
 
     public void onNewGameClick(View view) {
         startGame();
+        revealGrid();
+    }
+
+
+    public void revealGrid() {
+        for (int buttonIndex = 0; buttonIndex < mGameGrid.getChildCount(); buttonIndex++) {
+            ImageButton gridButton = (ImageButton) mGameGrid.getChildAt(buttonIndex);
+
+            //find the buttons row and col
+            int col = buttonIndex / MineGame.GRID_WIDTH;
+            int row = buttonIndex % MineGame.GRID_HEIGHT;
+
+            int tileValue = mGame.getTileValue(row, col);
+            switch (tileValue) {
+                case Tile.BOMB:
+                    gridButton.setImageBitmap(mSprites.get("bomb"));
+                    break;
+                case Tile.BLANK:
+                    gridButton.setImageBitmap(mSprites.get("empty"));
+                    break;
+                case 1:
+                    gridButton.setImageBitmap(mSprites.get("one"));
+                    break;
+                case 2:
+                    gridButton.setImageBitmap(mSprites.get("two"));
+                    break;
+                case 3:
+                    gridButton.setImageBitmap(mSprites.get("three"));
+                    break;
+                case 4:
+                    gridButton.setImageBitmap(mSprites.get("four"));
+                    break;
+                case 5:
+                    gridButton.setImageBitmap(mSprites.get("five"));
+                    break;
+                case 6:
+                    gridButton.setImageBitmap(mSprites.get("six"));
+                    break;
+                case 7:
+                    gridButton.setImageBitmap(mSprites.get("seven"));
+                    break;
+                case 8:
+                    gridButton.setImageBitmap(mSprites.get("eight"));
+                    break;
+                default: gridButton.setImageBitmap(mSprites.get("bomb_hit"));
+                    break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mobileappdev/myapplication/Tile.java
@@ -0,0 +1,40 @@
+package com.mobileappdev.myapplication;
+
+public class Tile {
+    public static final int BOMB = -1;
+    public static final int BLANK = 0;
+
+    private boolean isRevealed;
+    private boolean isFlagged;
+    private int value;
+
+    public Tile(int value){
+        this.value = value;
+        this.isRevealed = false;
+        this.isFlagged = false;
+    }
+
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        isRevealed = revealed;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
